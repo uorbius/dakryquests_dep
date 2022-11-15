108811a4df35dbb2baa66561ab01b0eae777d957
paul <lebedevpavel0511@gmail.com>
2022-11-16 00:17:54 +0100

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,16 +11,16 @@ function App() {
         <h2> All endpoints </h2>
         <br/>
         <div className="btns">
-          <a href="#">
+          <a href="https://serene-island-73390.herokuapp.com/api/film/get?offset=10&limit=20">
             paginate
           </a>
-          <a href="#">
+          <a href="https://serene-island-73390.herokuapp.com/api/film/get/5">
             get by id
           </a>
-          <a href="#">
+          <a href="https://serene-island-73390.herokuapp.com/api/film/get-all">
             get all 
           </a>
-          <a href="#">
+          <a href="https://serene-island-73390.herokuapp.com/api/film/search?query='test'">
             search by query
           </a>
         </div>
